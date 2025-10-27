bcf7cf90e6a3e2c294ba9941e7270053eecb8ea5
DOMS1279 <142471972+DOMS1279@users.noreply.github.com>
2025-10-27 13:11:59 +1100

    Dom S - Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 using namespace karger;
 
 int main() {
-    std::vector<Edge> edges = {
-        {0, 1}, {0, 2}, {1, 2}, {1, 3}, {2, 3}
+    std::vector<karger::Edge> edges = {
+        {0,1}, {0,2}, {1,2}, {1,3}, {2,3}
     };
     int n = 4;
 
-    int cut = minCutFixedPermutation(n, edges);
+    std::cout << "Running Randomised Karger:\n";
+    karger::minCutRandomised(n, edges, 123);
+
+    std::cout << "\nRunning Deterministic (Fixed Permutation):\n";
+    karger::minCutFixedPermutation(n, edges);
+
+    return 0;
 }
